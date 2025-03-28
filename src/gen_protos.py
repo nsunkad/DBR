84981b10bdec0c1ceebc75983de592ddd3748553
Aydan Pirani <aydanpirani@gmail.com>
2025-03-28 00:14:16 -0500

    removed generated from gitignore

--- src/gen_protos.py
+++ src/gen_protos.py
@@ -1,21 +1,28 @@
+# gen_database_proto.py
+
 import os
 import sys
 from grpc_tools import protoc
 
-def main():
+TARGETS = ["database.proto", "dbr.proto"]
+OUT_DIR = "generated"
+
+def gen_protos():
     # Compute the absolute path to the protos folder (one level up from application)
-    proto_include = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "protos"))
-    # Full path to the proto file
-    proto_file = os.path.join(proto_include, "dbr.proto")
+    proto_include = os.path.abspath(os.path.join(os.path.dirname(__file__), "protos"))
+    proto_files = [os.path.join(proto_include, target) for target in TARGETS]
+
+    out_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), OUT_DIR))
+    os.makedirs(out_dir, exist_ok=True)
     
     # Build the argument list similar to:
     # python -m grpc_tools.protoc -I../protos --python_out=. --grpc_python_out=. ../protos/database.proto
     protoc_args = [
         "",  # Dummy entry for the program name
         f"-I{proto_include}",
-        "--python_out=.",
-        "--grpc_python_out=.",
-        proto_file,
+        f"--python_out={out_dir}",
+        f"--grpc_python_out={out_dir}",
+        *proto_files,
     ]
     
     print("Running protoc with the following arguments:")
@@ -24,5 +31,6 @@ def main():
     if protoc.main(protoc_args) != 0:
         sys.exit("Error: protoc command failed.")
 
+
 if __name__ == "__main__":
-    main()
+    gen_protos()

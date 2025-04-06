0ee4c0221572994d2b4dfd08f685967297f8d053
Aydan Pirani <aydanpirani@gmail.com>
2025-04-05 23:01:30 -0500

    Added docker push

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 set -e
+cd $ROOT_DIR
+
+python3 -m venv $ROOT_DIR/src/venv
+source venv/bin/activate
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
+pip3 install wheel
+pip3 install -r requirements.txt
+
+
+cd $ROOT_DIR/src
 
 # Run the latency configuration script (adjust parameters as needed)
-python3 /usr/local/bin/set_latencies.py \
-    --interface eth0 \
-    --latencies-csv /etc/latency/region_latencies.csv \
-    --hostname-region-csv /etc/latency/hostname_regions.csv
-
+python3 -m scripts.gen_protos
+python3 -m scripts.set_latencies --interface eth0
 echo "Latency configuration applied. Container will now remain running."
 
-# Keep the container alive indefinitely
+python3 -m execution.service &
+python3 -m orchestration.service &
+echo "Execution and orchestration servers started. Container will now remain running."
+
+# # Keep the container alive indefinitely
 tail -f /dev/null

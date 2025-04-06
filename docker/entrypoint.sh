d9210757d7123fdaba9a225cdb5ff9b9c29d4879
Aydan Pirani <aydanpirani@gmail.com>
2025-04-06 15:09:37 -0500

    more testing changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,23 +4,23 @@ set -e
 echo "ROOT_DIR is: $ROOT_DIR"
 cd $ROOT_DIR
 
-python3 -m venv $ROOT_DIR/venv
+python -m venv $ROOT_DIR/venv
 . $ROOT_DIR/venv/bin/activate
-pip3 install --upgrade pip
-pip3 install --upgrade setuptools
-pip3 install wheel
-pip3 install -r requirements.txt
+pip install --upgrade pip
+pip install --upgrade setuptools
+pip install wheel
+pip install -r requirements.txt
 
 
 cd $ROOT_DIR/src
 
 # Run the latency configuration script (adjust parameters as needed)
-python3 -m scripts.gen_protos
-python3 -m scripts.set_latencies --interface eth0
+python -m scripts.gen_protos
+python -m scripts.set_latencies --interface eth0
 echo "Latency configuration applied. Container will now remain running."
 
-python3 -m execution.service &
-python3 -m orchestration.service &
+python -m execution.service &
+python -m orchestration.service &
 echo "Execution and orchestration servers started. Container will now remain running."
 
 # # Keep the container alive indefinitely

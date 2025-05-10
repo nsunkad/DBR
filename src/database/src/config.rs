866b55ab799982a764f25f152e8fd7635ebe0828
Aydan Pirani <aydanpirani@gmail.com>
2025-05-10 18:55:51 -0500

    smaller test framework and no write propagation

--- src/database/src/config.rs
+++ src/database/src/config.rs
@@ -1,4 +1,4 @@
-pub const PROPAGATE_WRITE: bool = true;
+pub const PROPAGATE_WRITE: bool = false;
 pub const NUM_INSTANCES: u64 = 10;
 pub const REPLICATION_FACTOR: u64 = 2;
 
--- src/database/src/service.rs
+++ src/database/src/service.rs
@@ -6,6 +6,7 @@ use std::sync::Arc;
 use crate::kv_store::InMemoryStore;
 use crate::types::Bytes;
 use crate::config::{NUM_INSTANCES, REPLICATION_FACTOR, PROPAGATE_WRITE};
+use os_info::OSInfo;
 
 use database::database_server::{Database};
 use database::{
@@ -48,40 +49,20 @@ impl Database for DB {
     async fn set(&self, request: Request<SetRequest>) -> Result<Response<SetReply>, Status> {
         let req = request.into_inner();
 
+        let os = OSInfo::new();
         let key_clone = req.key.clone();
         let value_clone = req.value.clone();
 
         let key = Bytes(key_clone.clone());
         let value = Bytes(value_clone.clone());
 
-        self.store.set(&key, &value).await;
+        let hostname = os.hostname().unwrap();
         
-        // Propagate write
-        if PROPAGATE_WRITE {
-            let read_regions_response = self.get_read_regions(Request::new(RegionRequest { key: key_clone.clone() })).await?;
-            let replica_regions = read_regions_response.into_inner().regions;
-
-            for i in 1..replica_regions.len() {            
-                let key = key_clone.clone();
-                let value = value_clone.clone();
-                let replica_clone = replica_regions[i].clone();
-
-                tokio::spawn(async move {
-                    match database::database_client::DatabaseClient::connect(replica_clone).await {
-                        Ok(mut client) => {
-                            let _ = client.set(Request::new(SetRequest {
-                                key: key,
-                                value: value,
-                            })).await;
-                        },
-                        Err(e) => {
-                            eprintln!("Failed to connect to replica: {}", e);
-                        }
-                    }
-                });
-            }
+        if hostname != self.get_write_region(&key) {
+            return Ok(Response::new(SetReply { success: false }));
         }
 
+        self.store.set(&key, &value).await;
 
         Ok(Response::new(SetReply { success: true }))
     }
@@ -90,19 +71,8 @@ impl Database for DB {
         println!("Received get_read_regions request");
         let key: Bytes = request.into_inner().key.into();
         
-        let mut hasher = DefaultHasher::new();
-        key.hash(&mut hasher);
-        let hash_value = hasher.finish();
-
-        const OFFSET: u64 = 1;
-        
-        // Get the three regions for the key
-        let regions = vec![
-            self.vms[((hash_value + 0*OFFSET) % NUM_INSTANCES) as usize].clone(),
-            self.vms[((hash_value + 1*OFFSET) % NUM_INSTANCES) as usize].clone(),
-            self.vms[((hash_value + 2*OFFSET) % NUM_INSTANCES) as usize].clone(),
-            self.vms[((hash_value + 3*OFFSET) % NUM_INSTANCES) as usize].clone(),
-        ];
+        let region = self.get_region(&key);
+        let regions = vec![region];
 
         println!("Read regions: {:?}", regions);
         Ok(Response::new(ReadRegionReply { regions }))
@@ -111,14 +81,18 @@ impl Database for DB {
     async fn get_write_region(&self, request: Request<RegionRequest>) -> Result<Response<WriteRegionReply>, Status> {
         println!("Received get_write_region request");
         let key: Bytes = request.into_inner().key.into();
-        
+        let region = self.get_region(&key);
+
+        println!("Write region: {}", region);
+
+        Ok(Response::new(WriteRegionReply { region }))        
+    }
+
+    fn get_region(&self, key: &Bytes) -> String {
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
         let hash_value = hasher.finish();
 
-        let region = self.vms[(hash_value % NUM_INSTANCES) as usize].clone();
-        println!("Write region: {}", region);
-
-        Ok(Response::new(WriteRegionReply { region }))        
+        self.vms[(hash_value % NUM_INSTANCES) as usize].clone()
     }
 }
